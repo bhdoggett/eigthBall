8b82a148192da2f556d87a44e680068247401126
bhdoggett <55600814+billy230@users.noreply.github.com>
2024-08-16 16:41:45 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+var randomNumber = Math.floor(Math.random() * 8);
+var eightBall
+
+switch (randomNumber) {
+    case 1:
+        eightBall = 'It is certain';
+        break
+    case 2:
+        eightBall = 'It is decidedly so';
+        break
+    case 3:
+        eightBall = 'Reply hazy try again';
+        break
+    case 4:
+        eightBall = 'Cannot predict now';
+        break
+    case 5:
+        eightBall = 'Do not count on it';
+        break
+    case 6:
+        eightBall = 'My sources say no';
+        break
+    case 7:
+        eightBall = 'Outlook not so good';
+        break
+    default:
+        eightBall = 'Signs point to yes';
+}
+
+console.log(eightBall)
+
+// if (randomNumber === 1) {
+//     eightBall = 'It is certain';
+// } else if (randomNumber === 2) {
+//     eightBall = 'It is decidedly so';
+// } else if (randomNumber === 3) {
+//     eightBall = 'Reply hazy try again';
+// } else if (randomNumber === 4) {
+//     eightBall = 'Cannot predict now';
+// } else if (randomNumber === 5) {
+//     eightBall = 'Do not count on it';
+// } else if (randomNumber === 6) {
+//     eightBall = 'My sources say no';
+// } else if (randomNumber === 7) {
+//     eightBall = 'Outlook not so good';
+// } else if (randomNumber === 8) {
+//     eightBall = 'Signs point to yes';
+// }
+
+// console.log(eightBall)
+
+
